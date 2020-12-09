805407c13e3b4b8a7c4644425967f2028cbc0903
Капинус Юлия <kapinusjuli55@gmail.com>
2020-12-09 21:53:59 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import menuCardTmpl from "./templates/picture-card.hbs";
 
 import ImgApiService from "./js/components/img-service.js";
 import debounce from "lodash.debounce";
+import LoadMoreBtn from "./js/components/load-more-btn";
 
 const refs = {
   searhForm: document.querySelector(".js-search-form"),
@@ -17,34 +18,58 @@ console.log(refs.searchBtn);
 
 const ImageApiService = new ImgApiService();
 
-// const loadMoreBtn = new LoadMoreBtn({
-//   selector: '[data-action="load-more"]',
-//   hidden: true,
-// });
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 
 refs.searhForm.addEventListener("submit", onSearch);
-
+refs.searhForm.addEventListener("submit", scrollTo);
 // refs.searhForm.addEventListener("input", debounce(onSearch, 500));
-refs.loadMoreBtn.addEventListener("click", onLoadMore);
+loadMoreBtn.refs.button.addEventListener("click", onLoadMore);
+loadMoreBtn.refs.button.addEventListener("click", scrollTo);
 
+// переписываю функцию он серч
 function onSearch(e) {
   e.preventDefault();
+  // clearImageContainer();
 
   ImageApiService.query = e.currentTarget.elements.query.value;
-  // ImageApiService.resetPage();
+  console.log(ImgApiService.query);
 
-  ImageApiService.fetchImg().then(appendImageMarkup);
+  loadMoreBtn.show();
+  ImageApiService.resetPage();
+  clearImageContainer();
+  // loadMoreBtn.disable();
+
+  onLoadMore();
 }
 
-//
-
 function onLoadMore() {
-  //   ImageApiService.query = e.currentTarget.elements.query.value;
-  ImageApiService.fetchImg().then(appendImageMarkup);
+  loadMoreBtn.disable();
+  ImageApiService.fetchImg().then((hits) => {
+    appendImageMarkup(hits);
+    loadMoreBtn.enable();
+  });
 }
 
 function appendImageMarkup(hits) {
   refs.galleryContainer.insertAdjacentHTML("beforeend", menuCardTmpl(hits));
+  if (hits.length < 12) {
+    loadMoreBtn.hide();
+  }
+}
+
+function clearImageContainer() {
+  refs.galleryContainer.innerHTML = "";
+}
+
+function scrollTo() {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 //  cat
--- src/js/components/img-service.js
+++ src/js/components/img-service.js
@@ -1,19 +1,26 @@
+const API_KEY = "19196344-39ec0398b9669a2b5bdda7f21";
+
 export default class ImgApiService {
   constructor() {
     this.searchQuery = "";
     this.page = 1;
   }
   fetchImg() {
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=19196344-39ec0398b9669a2b5bdda7f21`;
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
 
     return fetch(url)
       .then((r) => r.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        // console.log(data.hits.length);
         this.page += 1;
         return data.hits;
       });
   }
+  resetPage() {
+    this.page = 1;
+  }
+
   get query() {
     return this.searchQuery;
   }
